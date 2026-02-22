99238b0eccc5be4b096d6a6b45705c729793863a
Abhay Chaudhary <abhaycdry10@gmail.com>
2026-02-22 14:43:49 +0545

    changes added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const cookieParser = require('cookie-parser');
-const { checkUser } = require('./middleware/authMiddleware');
-const dbConnect = require('./config/db');
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { checkUser } = require("./middleware/authMiddleware");
+const dbConnect = require("./config/db");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
@@ -12,26 +12,23 @@ const app = express();
 dbConnect();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // middleware to parse JSON request bodies
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));                // parses form data "For HTML data"
+app.use(express.urlencoded({ extended: true })); // parses form data "For HTML data"
 app.use(cookieParser());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(authRoutes);  //use authRoutes from routes folder for routing
-
-// middleware and routes
-app.get('*', checkUser);        //checkUser = checks if a user is logged in and sets user info for the frontend.
-app.get('/', (req, res) => res.render('home'));
-app.get('/icecreams', (req, res) => 
-    res.render('icecreams')
-);
+// routes
+app.get("*", checkUser); //checkUser = checks if a user is logged in and sets user info for the frontend.
+app.get("/", (req, res) => res.render("home"));
+app.get("/icecreams", (req, res) => res.render("icecreams"));
+app.use(authRoutes); //use authRoutes from routes folder for routing
 
 const PORT = process.env.PORT || 4040;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,29 @@
 const User = require("../models/User");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // handle errors                                        //this can be skipped. written because it looks good on postman.
 const handleErrors = (err) => {
   console.log(err.message, err.code);
-  let errors = { email: '', password: '' };
+  let errors = { email: "", password: "" };
 
   //incorrect email
-  if (err.message === 'incorrect email'){
-    errors.email = 'that email is not registered';
+  if (err.message === "incorrect email") {
+    errors.email = "that email is not registered";
   }
 
-//incorrect password
-if (err.message === 'incorrect password'){
-  errors.email = 'your password is incorrect';
-}
+  //incorrect password
+  if (err.message === "incorrect password") {
+    errors.email = "your password is incorrect";
+  }
 
   // duplicate email error
   if (err.code === 11000) {
-    errors.email = 'that email is already registered';
+    errors.email = "that email is already registered";
     return errors;
   }
 
   // validation errors
-  if (err.message.includes('user validation failed')) {
+  if (err.message.includes("user validation failed")) {
     // console.log(err);
     Object.values(err.errors).forEach(({ properties }) => {
       // console.log(val);
@@ -33,67 +33,65 @@ if (err.message === 'incorrect password'){
   }
 
   return errors;
-}
-
+};
 
 //for jsonwebtoken
 const maxAge = 3 * 24 * 60 * 60;
-const createToken = (id)=>{
-  return jwt.sign({id}, 'Abhay secret',{                             //jatibela user create garinxa tyo wala id ho.
-    expiresIn: maxAge
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY || "AbhaySecret", {
+    //jatibela user create garinxa tyo wala id ho.
+    expiresIn: maxAge,
   });
-}
+};
 
 // controller actions
 module.exports.signup_get = (req, res) => {
-  const user = req.user || null;
-  res.render('signup', { user });
-}
-
-module.exports.login_get = (req, res) => {
-  const user = req.user || null;                    // assuming 'req.user' contains the logged-in user's info
-  res.render('login', { user });
+  res.render("signup");
 };
 
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
 
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.create({ email, password });     //we need jsonwebtoken here         //id = user
+    const user = await User.create({ email, password }); //we need jsonwebtoken here         //id = user
     const token = createToken(user._id);
-    res.cookie('jwt', token, {httpOnly:true, maxAge: maxAge * 1000});
-    res.status(201).json({user: {
-     id: user._id,
-     email: user.email}
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({
+      user: {
+        id: user._id,
+        email: user.email,
+      },
     });
+  } catch (err) {
+    const errors = handleErrors(err); //console.log(err);
+    res.status(400).json({ errors }); //res.status(400).send('error, user not created');
   }
-  catch(err) {
-    const errors = handleErrors(err);               //console.log(err);
-    res.status(400).json({ errors });               //res.status(400).send('error, user not created');
-  }
- 
-}
+};
 
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
-  try{
-    const user = await User.login(email,password);
+  try {
+    const user = await User.login(email, password);
     const token = createToken(user._id);
-    res.cookie('jwt', token, {httpOnly:true, maxAge: maxAge * 1000});
-    res.status(200).json({user: {
-      id: user._id,
-      email: user.email }
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({
+      user: {
+        id: user._id,
+        email: user.email,
+      },
     });
-  }
-  catch(err){
+  } catch (err) {
     const errors = handleErrors(err);
-    res.status(400).json({});
+    res.status(400).json({ errors });
   }
-}
+};
 
 module.exports.logout_get = (req, res) => {
-  res.cookie('jwt', '', {maxAge: 1});
-  res.redirect('/');
-}
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
